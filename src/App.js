c6b2ad5488c1e8dc4774cee43128548f5e33d57e
heshaniadithya <heshaniadithya2006@gmail.com>
2024-04-23 01:11:00 +0530

    learn-with-podcasts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import { jwtDecode } from "jwt-decode";
+import { UserContext } from "./contexts/UserContext";
+import Header from "./components/Header";
+import Episode from "./components/Episode";
 
 function App() {
+  const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [data, setData] = useState([]);
+  const signInButton = useRef();
+
+  const handleCallBack = (res) => {
+    let user = jwtDecode(res.credential);
+    setUser(user);
+    setLoggedIn(true);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "846767556832-fs88dpq2a8dh7915pns965v40q3h9ov8.apps.googleusercontent.com",
+      callback: handleCallBack
+    });
+    google.accounts.id.renderButton(
+      signInButton.current,
+      { theme: "outline", size: "large" }
+    )
+  }, [loggedIn])
+
+  const rssFeed = "https://cdn.atp.fm/rss/public?m2swoudx";
+  useEffect(() => {
+      fetch(rssFeed)
+      .then(res => res.text())
+      .then(str => {
+        const parser = new window.DOMParser();
+        const data = parser.parseFromString(str, "text/xml");
+        const itemList = data.querySelectorAll("item");
+        const items = [];
+        itemList.forEach(el => {
+          items.push({
+            title: el.querySelector("title").innerHTML,
+            pubDate: new Date(el.querySelector("pubDate").textContent).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" }),
+            mp3: el.querySelector("enclosure").getAttribute("url"),
+            link: el.querySelector("link").innerHTML 
+        })
+          })
+          setData(items)
+        })
+  }, [rssFeed])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <UserContext.Provider value={[user, setUser]}>
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} signInButton={signInButton} />
+      {loggedIn ? (
+        <div className="pl-4 py-4 flex flex-col items-center">
+        <h2 className="text-2xl font-medium">Accidental Tech Podcast</h2>
+      {data.map((ep, i) =>
+      <Episode 
+      key={i}
+      title={ep.title}
+      pubDate={ep.pubDate}
+      link={ep.link}
+      mp3={ep.mp3}
+      />
+    )}
     </div>
+      ) : <h3 className="text-center text-2xl mx-auto px-5 pt-6">Something to display here, some promotional picture or text</h3>}
+      
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Episode.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+
+const Episode = ({title, pubDate, link, mp3 }) => {
+
+    const [user] = useContext(UserContext);
+
+    return ( 
+        <div className="max-w-3xl flex flex-row items-center border rounded-lg my-4">
+            <div className="w-1/2 pl-2 mt-2 mb-4 py-2 px-5 mx-auto">
+                <a href={link} target="_blank" rel="noopener noreferrer"
+                    className="underline hover:opacity-70"
+                >
+                    <p>{title}</p>
+                </a>
+                <audio src={mp3} className="my-4 px-2" controls />
+                <p>{pubDate}</p>
+            </div>
+            <div className="w-1/2 flex flex-col gap-1 my-2 px-8">
+                <label htmlFor="notes" className="text-gray-700 font-medium">Make Notes Here, {user.given_name}</label>
+                <textarea
+                   id="notes"
+                   className="border rounded-lg p-2"
+                   placeholder="What did you learn from the episode?"
+                   rows={5}
+               />
+            </div>
+            </div>
+     );
+}
+ 
+export default Episode;
